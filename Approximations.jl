3a2001364b718955e681469917265a3e5253b906
Simon Jacobsson <simonj@beltrami.dyn.cs.kuleuven.be>
2023-07-04 18:58:19 +0200

    PlotErrors.jl now works similarly as the one in vector-valued approximations. Also I made a Mathematica notebook that computes the n first derivatives of an inverse function, to use when bounding the error of approximating the SVD.

--- Approximations.jl
+++ Approximations.jl
@@ -23,9 +23,8 @@ include("../vector-valued approximations/Approximations.jl") # TODO: Make Approx
 Approximate a manifold-valued function using Riemann normal coordinate chart.
 """
 function approximate(#={{{=#
-    M::AbstractManifold,
     m::Int,
-    n::Int,
+    M::AbstractManifold,
     f::Function; # :: [-1, 1]^m -> M^n
     base_approximate=approximate_vector::Function, # :: (m::Int) -> (n::Int) -> ([-1, 1]^m -> R^n) -> ([-1, 1]^m -> R^n)
     # TODO: chart?
@@ -43,7 +42,7 @@ function approximate(#={{{=#
     chart_inv = (X -> exp(M, p, X)) ∘ (X -> get_vector(M, p, X, B)) # get_vector :: R^n -> T_p M, exp : T_p M -> M^n
 
     g = chart ∘ f
-    ghat = base_approximate(m, n, g; kwargs...)
+    ghat = base_approximate(m, manifold_dimension(M), g; kwargs...)
     fhat = chart_inv ∘ ghat
     return fhat
 end#=}}}=#
--- plot errors/PlotErrors.jl
+++ plot errors/PlotErrors.jl
@@ -6,6 +6,8 @@ using Plots
 using LinearAlgebra
 using Random
 using Printf
+using CSV
+using DataFrames: DataFrame
 
 #################### Plot approximation ####################
 # ENV["MPLBACKEND"] = "TkAgg" ;# Solves "Warning: No working GUI backend found for matplotlib"
@@ -89,15 +91,65 @@ end#=}}}=#
 
 Lambda(N) = (2 / pi) * log(N + 1) + 1
 
-# TODO: Use LowRankApprox...
-function closest_rank_one_matrix(;#={{{=#
-    m = 4,
-    n1 = 40,
-    n2 = 60,
-    Ns=2:2:13,
+function plot_errors(#={{{=#
+    f::Function, # :: [-1, 1]^m -> M^n
+    b::Function, # :: N -> R Error bound
+    m::Int64,
+    M::AbstractManifold,
+    Ns;
     verbose=false,
-    savefigure=false,
-    figkwargs=(windowsize=(240, 160), guidefontsize=5, xtickfontsize=5, ytickfontsize=5, legendfontsize=5),
+    save=false,
+    get_univariate_scheme=chebfun, # Int -> UnivariateApproximationScheme
+    kwargs...
+    )
+
+    es = [NaN for _ in Ns]
+    bs = [NaN for _ in Ns]
+    global fhat
+    for (i, N) = enumerate(Ns)
+        if verbose; println(i, "/", length(Ns)); end
+    
+        fhat = approximate(
+            m,
+            M,
+            f;
+            univariate_scheme=get_univariate_scheme(N),
+            kwargs...
+            )
+
+        # TODO: calculate max betterly
+        es[i] = maximum([
+            distance(M, f(x), fhat(x))
+            for x in [2 * rand(m) .- 1.0 for _ in 1:1000]])
+        bs[i] = b(N)
+        if verbose; println("error ", es[i]); end
+    end
+    
+    p = plot(Ns, bs;
+        label="error bound",
+        xlabel="N",
+        xticks=Ns,
+        yaxis=:log,
+        ylims=(1e-16, 2 * maximum([es..., bs...])),
+        yticks=([1e0, 1e-5, 1e-10, 1e-15]),
+        legend=:topright,
+        )
+    scatter!(Ns, es;
+        label="measured error")
+    if save
+        savefig(string(f, ".pdf"))
+        CSV.write(string(f, ".csv"), DataFrame([:Ns => Ns, :es => es, :bs => bs]))
+    end
+    display(p)
+end#=}}}=#
+
+# TODO: Use LowRankApprox...
+# Use tol=1e-15 to unlock full precision
+function closest_rank_one_matrix(;#={{{=#
+    m=4,
+    n1=20,
+    n2=30,
+    Ns=2:1:13,
     kwargs...
     )
 
@@ -105,19 +157,20 @@ function closest_rank_one_matrix(;#={{{=#
     M = Segre((n1, n2))
     
     Random.seed!(420)
-    a = LinearAlgebra.normalize(rand(n1))
-    b = LinearAlgebra.normalize(rand(n2))
+    A0 = LinearAlgebra.normalize(rand(n1)) * LinearAlgebra.normalize(rand(n2))'
     As = [LinearAlgebra.normalize(rand(n1, n2)) for _ in 1:m]
-    function f(x) # : [-1, 1]^m -> Segre((m1, m2))
+    function closest_rank_one_matrix(x) # : [-1, 1]^m -> Segre((m1, m2))
         U, S, Vt = svd(
             sum([xi * A for (xi, A) in zip(x, As)]) + 
-            2 * m * a * b'
+            2 * m * A0
             )
         return [[S[1]], U[:, 1], Vt[:, 1]]
     end
 
+    f = closest_rank_one_matrix
     # nth derivative of the svd along axis l #={{{=#
-    # https://www.jstor.org/stable/2695472
+    # Computed in higher_order_derivatives_of_inverse_function.nb
+    # See https://www.jstor.org/stable/2695472
     f1(l) = big(norm(M, f(zeros(m)), finite_difference(t -> f([i == l ? t : 0 for i in 1:m]), 0.0, 1e-5, order=1)))
     g1(l) = 1 / f1(l)
     f2(l) = big(norm(M, f(zeros(m)), finite_difference(t -> f([i == l ? t : 0 for i in 1:m]), 0.0, 1e-4, order=2)))
@@ -167,63 +220,41 @@ function closest_rank_one_matrix(;#={{{=#
         75675600 * g1(l)^2 * g2(l)^5 * g3(l)^2 +
         21021000 * g1(l)^3 * g2(l)^3 * g3(l)^3 -
         1401400 * g1(l)^4 * g2(l) * g3(l)^4
-        ))#=}}}=#
+        ))
+    V11(l) = 2 * abs(25025  / g1(l)^21 * (
+        26163 * g2(l)^10
+        - 78489 * g1(l) * g2(l)^8 * g3(l)
+        + 77112 * g1(l)^2 * g2(l)^6 * g3(l)^2
+        - 28560 * g1(l)^3 * g2(l)^4 * g3(l)^3
+        + 3360 * g1(l)^4 * g2(l)^2 * g3(l)^4
+        - 56 * g1(l)^5 * g3(l)^5
+        ))
+        #=}}}=#
 
-    V(nu) = maximum([[V1, V2, V3, V3, V4, V5, V6, V7, V8, V9, V10, repeat([t -> NaN], 100)...][nu](l) for l in 1:m])
+    V(nu) = maximum([[V1, V2, V3, V3, V4, V5, V6, V7, V8, V9, V10, V11, repeat([t -> NaN], 100)...][nu](l) for l in 1:m])
 
-    global fhat
-    es = [NaN for _ in Ns]
-    bs = [NaN for _ in Ns]
-    for (i, N) = enumerate(Ns)
-        if verbose; println(i, "/", length(Ns)); end
+    # epsilon is the error bound on the tangent space for tensorized Chebyshev interpolation
+    # b is the error bound on the manifold
+    sigma = maximum([
+        distance(M, f(zeros(m)), f(x))
+        for x in [2 * rand(m) .- 1.0 for _ in 1:1000]])
+    H = -1.0 / m
+    epsilon(N) = minimum([
+        4 * V(nu) * (Lambda(N)^m - 1) / (pi * nu * big(N - nu)^nu * (Lambda(N) - 1))
+        for nu in 1:(N - 1)])
+    b(N) = epsilon(N) + 2 / sqrt(abs(H)) * asinh(epsilon(N) * sinh(sqrt(abs(H)) * sigma) / (2 * sigma))
 
-        # Compute bound on manifold error
-        sigma = maximum([
-            distance(M, f(zeros(m)), f(x))
-            for x in [2 * rand(m) .- 1.0 for _ in 1:1000]])
-        H = -1.0 / m
-        epsilon = minimum([
-            4 * V(nu) * (Lambda(N)^m - 1) / (pi * nu * big(N - nu)^nu * (Lambda(N) - 1))
-            for nu in 1:(N - 1)])
-        bs[i] = epsilon + 2 / sqrt(abs(H)) * asinh(epsilon * sinh(sqrt(abs(H)) * sigma) / (2 * sigma))
-
-        fhat = approximate(M, m, n, f; res=N, kwargs...)
-
-        # TODO: calculate max betterly
-        es[i] = maximum([
-            distance(M, f(x), fhat(x))
-            for x in [2 * rand(m) .- 1.0 for _ in 1:1000]])
-
-        if verbose
-            println("error ", es[i])
-            x = ones(m) - 2.0 * rand(m)
-            print("evaluating f     ")
-            @time(f(x))
-            print("evaluating fhat  ")
-            @time(fhat(x))
-            println()
-        end
-    end
-
-    # plot(Ns, bs; yaxis=:log, label="error bound", xlabel="N")
-    p = plot(Ns, bs;
-        label="error bound",
-        xlabel="N",
-        xticks=Ns,
-        yaxis=:log,
-        ylims=(1e-16, 2 * maximum([es..., bs...])),
-        yticks=([1e0, 1e-5, 1e-10, 1e-15]),
-        figkwargs...
+    plot_errors(
+        closest_rank_one_matrix,
+        b,
+        m,
+        M,
+        Ns;
+        kwargs...
         )
-    scatter!(Ns, es;
-        label="measured error",
-        color=2,
-        markersize=3,
-        )
-    if savefigure; savefig("closest_rank_one_matrix.pdf"); end
-    display(p)
 end#=}}}=#
 
+# TODO: Probably I will have to implement exponential and logarithmic maps etc for the Grassmannian myself
 function nullspace(;#={{{=#
     m = 4,
     n1 = 40,
