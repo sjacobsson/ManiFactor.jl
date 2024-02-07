ec4a96c3b217db4c6a1dce7e70103ff6bb598f18
Simon Jacobsson <simonj@beltrami.dyn.cs.kuleuven.be>
2024-02-07 13:47:51 +0100

    Added option to specify p manually

--- src/ManiFactor.jl
+++ src/ManiFactor.jl
@@ -15,6 +15,7 @@ export
         m::Int,
         M::AbstractManifold,
         f::Function; # :: [-1, 1]^m -> M^n
+        p=[], # Point to linearize around. Default is Karcher mean of 100 samples.
         base_approximate=approximate_vector::Function, # :: (m::Int) -> (n::Int) -> ([-1, 1]^m -> R^n) -> ([-1, 1]^m -> R^n)
         kwargs...
         )::Function # :: [-1, 1]^m -> M^n
@@ -25,17 +26,19 @@ function approximate(#={{{=#
     m::Int,
     M::AbstractManifold,
     f::Function; # :: [-1, 1]^m -> M^n
+    p=[], # Point to linearize around. Default is Karcher mean of 100 samples.
     base_approximate=approximate_vector::Function, # :: (m::Int) -> (n::Int) -> ([-1, 1]^m -> R^n) -> ([-1, 1]^m -> R^n)
     # TODO: provice chart as an option?
     kwargs...
     )::Function # :: [-1, 1]^m -> M^n
 
-    # Evaluate f on a point cloud in [-1, 1]^m
-    xs = [2.0 * rand(m) .- 1.0 for _ in 1:100]
-    fs = f.(xs)
     
     # Choose a point on M and linearize from there
-    p = mean(M, fs)
+    if p==[]
+        # Evaluate f on a point cloud in [-1, 1]^m
+        xs = [2.0 * rand(m) .- 1.0 for _ in 1:100]
+        p = mean(M, f.(xs))
+    end
     B = DefaultOrthonormalBasis()
     chart = (X -> get_coordinates(M, p, X, B)) ∘ (q -> log(M, p, q)) # log :: M -> T_p M, get_coordinates : T_p M -> R^n
     chart_inv = (X -> exp(M, p, X)) ∘ (X -> get_vector(M, p, X, B)) # get_vector :: R^n -> T_p M, exp : T_p M -> M^n
