c5b7338b2e71672b808a1575dbebcacdee97e140
Simon Jacobsson <simon.jacobsson@kuleuven.be>
2025-05-06 20:54:15 +0200

    make plot with timings for every N

--- examples/Example4.jl
+++ examples/Example4.jl
@@ -9,6 +9,7 @@ using Preconditioners
 
 using Random; Random.seed!(1)
 using Plots; pyplot()
+using BenchmarkTools; benchmark = false # Set to true if you want to get timings aswell (sloow)
 
 m = 2
 Ns = 2:1:14
@@ -51,9 +52,10 @@ end
 # Loop over nbr of interpolation points
 xs = [2 * rand(m) .- 1.0 for _ in 1:100]
 p = mean(M, f.(xs))
-es0 = [NaN for _ in Ns]
+es0 = [NaN for _ in Ns] # Errors
+ts0 = [NaN for _ in Ns] # Timings
 for (i, N) = enumerate(Ns)
-    local fhat = approximate(
+    fhat = approximate(
         m,
         M,
         f;
@@ -61,10 +63,15 @@ for (i, N) = enumerate(Ns)
         univariate_scheme=chebyshev(N),
         )
 
-    local g = (X -> get_coordinates(M, p, X, DefaultOrthonormalBasis())) ∘ (x -> log(M, p, f(x)))
-    local ghat = get_ghat(fhat)
+    g = (X -> get_coordinates(M, p, X, DefaultOrthonormalBasis())) ∘ (x -> log(M, p, f(x)))
+    ghat = get_ghat(fhat)
 
     es0[i] = maximum([distance(M, f(x), fhat(x)) for x in xs])
+
+    if benchmark
+        bmark = @benchmark $fhat(xs[1])
+        ts0[i] = minimum(bmark).time * 1e-6 # [ms]
+    end
 end
 plt = plot(
     xlabel="N",
@@ -79,8 +86,9 @@ display(plt)
 
 # Loop over nbr of interpolation points
 es1 = [NaN for _ in Ns]
+ts1 = [NaN for _ in Ns]
 for (i, N) = enumerate(Ns)
-    local fhat = approximate(
+    fhat = approximate(
         m,
         M,
         f;
@@ -90,18 +98,24 @@ for (i, N) = enumerate(Ns)
         univariate_scheme=chebyshev(N),
         )
 
-    local g = (X -> get_coordinates(M, p, X, DefaultOrthonormalBasis())) ∘ (x -> log(M, p, f(x)))
-    local ghat = get_ghat(fhat)
+    g = (X -> get_coordinates(M, p, X, DefaultOrthonormalBasis())) ∘ (x -> log(M, p, f(x)))
+    ghat = get_ghat(fhat)
 
     es1[i] = maximum([distance(M, f(x), fhat(x)) for x in xs])
+
+    if benchmark
+        bmark = @benchmark $fhat(xs[1])
+        ts1[i] = minimum(bmark).time * 1e-6 # [ms]
+    end
 end
 scatter!(plt, Ns, es1; label="error using QR retraction")
 display(plt)
 
 # Loop over nbr of interpolation points
 es2 = [NaN for _ in Ns]
+ts2 = [NaN for _ in Ns]
 for (i, N) = enumerate(Ns)
-    local fhat = approximate(
+    fhat = approximate(
         m,
         M,
         f;
@@ -111,10 +125,15 @@ for (i, N) = enumerate(Ns)
         univariate_scheme=chebyshev(N),
         )
 
-    local g = (X -> get_coordinates(M, p, X, DefaultOrthonormalBasis())) ∘ (x -> log(M, p, f(x)))
-    local ghat = get_ghat(fhat)
+    g = (X -> get_coordinates(M, p, X, DefaultOrthonormalBasis())) ∘ (x -> log(M, p, f(x)))
+    ghat = get_ghat(fhat)
 
     es2[i] = maximum([distance(M, f(x), fhat(x)) for x in xs])
+
+    if benchmark
+        bmark = @benchmark $fhat(xs[1])
+        ts2[i] = minimum(bmark).time * 1e-6 # [ms]
+    end
 end
 scatter!(plt, Ns, es2; label="error using polar retraction")
 display(plt)
@@ -123,43 +142,15 @@ display(plt)
 # using CSV
 # using DataFrames: DataFrame
 # savefig("Example4.png")
-# CSV.write("Example4.csv", DataFrame([:Ns => Ns, :es0 => es0, :es1 => es1, :es2 => es2]))
+# CSV.write("Example4.csv", DataFrame([:Ns => Ns, :es0 => es0, :es1 => es1, :es2 => es2, :ts0 => ts0, :ts1 => ts1, :ts2 => ts2]))
 
-# # To benchmark
-# using BenchmarkTools
-# fhat0 = approximate(
-#     m,
-#     M,
-#     f;
-#     p=p,
-#     univariate_scheme=chebyshev(10),
-#     reqrank=[5, 5, 5],
+# # To plot the timings
+# plt_ = plot(
+#     xlabel="N",
+#     xticks=Ns,
+#     ylims=(0.0, maximum([ts0..., ts1..., ts2...])),
+#     legend=:topright,
 #     )
-# fhat1 = approximate(
-#     m,
-#     M,
-#     f;
-#     p=p,
-#     exp=(M, p, X) -> retract(M, p, X, QRRetraction()),
-#     log=(M, p, X) -> inverse_retract(M, p, X, QRInverseRetraction()),
-#     univariate_scheme=chebyshev(10),
-#     reqrank=[5, 5, 5],
-#     )
-# fhat2 = approximate(
-#     m,
-#     M,
-#     f;
-#     p=p,
-#     exp=(M, p, X) -> retract(M, p, X, PolarRetraction()),
-#     log=(M, p, X) -> inverse_retract(M, p, X, PolarInverseRetraction()),
-#     univariate_scheme=chebyshev(10),
-#     reqrank=[5, 5, 5],
-#     )
-# print("f time: "); @btime f(rand(2)) # btime reports fastest time
-# println("fhat0 error: ", maximum([distance(M, f(x), fhat0(x)) for x in xs]))
-# print("fhat0 time: "); @btime fhat0(rand(2))
-# println("fhat1 error: ", maximum([distance(M, f(x), fhat1(x)) for x in xs]))
-# print("fhat1 time: "); @btime fhat1(rand(2))
-# println("fhat2 error: ", maximum([distance(M, f(x), fhat2(x)) for x in xs]))
-# print("fhat2 time: "); @btime fhat2(rand(2))
-# "" # BODGE
+# scatter!(plt_, Ns, ts0; label="time using exp")
+# scatter!(plt_, Ns, ts1; label="time using QR retraction")
+# scatter!(plt_, Ns, ts2; label="time using polar retraction")
